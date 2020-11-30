3ecc452da66e2e9dbfc85544e409e1d9ab4d642a
Seth Herz <sherz@uvm.edu>
2020-11-30 18:36:50 -0500

    Fixed some bugs, pressure analog read holding up code

--- resp_v3.ino
+++ resp_v3.ino
@@ -1,22 +1,23 @@
 #include <Arduino.h>
-#include <AccelStepper/AccelStepper.h>
-#include <LiquidCrystalI2C/LiquidCrystal_I2C.h>
-#include <Timer2/Timer.h>
-#include <Timer2/Event.h>
-#include <Wire/Wire.h>
-#include <ResponsiveAnalogRead/src/ResponsiveAnalogRead.h>
-#include <HX711/HX711.h>
+#include <AccelStepper.h>
+#include <LiquidCrystal_I2C.h>
+#include <Timer.h>
+#include <Event.h>
+#include <Wire.h>
+#include <ResponsiveAnalogRead.h>
+#include <HX711.h>
 
 HX711 pressure;
-enum pins = {pulse = 6, dir = 5, buttonA = 2, buttonB = 3, pressure = 8, clock = 9, diff = A5, buzzer = 11};
-AccelStepper stepper(1, pins.pulse, pins.dir);
+enum pins {pulsePin = 6, dir = 5, buttonA = 2, buttonB = 3, pressureSensor = 8, Clock = 9, diff = A5, buzzer = 11} pins;
+AccelStepper stepper(1, pulsePin, dir);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 Timer t;
-enum potPins = {POT1 = A0, POT2 = A1, POT3 = A2, POT4 = A3};
-enum POTS = {ResponsiveAnalogRead TIDALPEAK(potPins.POT1, true), ResponsiveAnalogRead RESPRATE(potPins.POT2, true),
-        ResponsiveAnalogRead IERATIO(potPins.POT3, true), ResponsiveAnalogRead TRIGGER(potPins.POT4, true)};
-enum constants = {MAXPRESSURE = 3919, FULLOPEN = 300, FLOW_MEASURE_INTERVAL = 30, CALIBRATION_FACTOR = 15000};
-
+enum potPins {POT1 = A0, POT2 = A1, POT3 = A2, POT4 = A3};
+ResponsiveAnalogRead TIDALPEAK(POT1, true);
+ResponsiveAnalogRead RESPRATE(POT2, true);
+ResponsiveAnalogRead IERATIO(POT3, true);
+ResponsiveAnalogRead TRIGGER(POT4, true);
+enum constants {MAXPRESSURE = 3919, FULLOPEN = 300, FLOW_MEASURE_INTERVAL = 30, CALIBRATION_FACTOR = 15000};
 bool potChanged = false;
 int flowTimer;
 int count;
@@ -27,7 +28,7 @@ typedef struct {
     float Tex;
     float Thold;
     float flowDesired;
-    bool inhale
+    bool inhale;
 } vars;
 vars VARS;
 
@@ -36,7 +37,7 @@ typedef struct {
     float diff;
     float flowRate;
     float totalVol;
-    float PEEP
+    float PEEP;
 } readings;
 
 readings READINGS;
@@ -57,21 +58,25 @@ float getDiff();
 void getFlow();
 
 void setup() {
-    pinMode(pins.buttonA, INPUT_PULLUP);
-    pinMode(pins.buttonB, INPUT_PULLUP);
-    pinMode(pins.diff, INPUT);
-    pinMode(potPins.POT1, INPUT);
-    pinMode(potPins.POT2, INPUT);
-    pinMode(potPins.POT3, INPUT);
-    pinMode(potPins.POT4, INPUT);
-    pinMode(pins.buzzer, OUTPUT);
-    pinMode(pins.pulse, OUTPUT);
-    pinMode(pins.dir, OUTPUT);
-    pressure.begin(pins.pressure, pins.clock);
+    pinMode(buttonA, INPUT_PULLUP);
+    pinMode(buttonB, INPUT_PULLUP);
+    pinMode(diff, INPUT);
+    pinMode(POT1, INPUT);
+    pinMode(POT2, INPUT);
+    pinMode(POT3, INPUT);
+    pinMode(POT4, INPUT);
+    pinMode(buzzer, OUTPUT);
+    pinMode(pulsePin, OUTPUT);
+    pinMode(dir, OUTPUT);
+    Serial.begin(9600);
+    Serial.print("HERE1");
+
+    pressure.begin(pressureSensor, Clock);
     pressure.set_scale();
     pressure.tare();
-    pressure.set_scale(constants.CALIBRATION_FACTOR)
-    lcd.init();
+    pressure.set_offset(CALIBRATION_FACTOR); // TODO: ???
+    Serial.print("here");
+    lcd.begin();
     lcd.backlight();
     lcd.clear();
     lcd.setCursor(0,0);
@@ -82,23 +87,23 @@ void setup() {
     lcd.print("I/E Ratio: 1/");
     lcd.setCursor(0,3);
     lcd.print("Trigger: ");
-    while (digitalRead(pins.buttonA)==HIGH) {
-        Serial.print("HERE");
+    while (digitalRead(buttonA)==HIGH) {
+        Serial.print("HERE3");
         lcd.setCursor(13, 0);
-        POTS.TIDALPEAK.update();
-        SETTINGS.tidalPeak = map(POTS.TIDALPEAK.getValue(), 0, 1023, 200, 800); //cc
+        TIDALPEAK.update();
+        SETTINGS.tidalPeak = map(TIDALPEAK.getValue(), 0, 1023, 200, 800); //cc
         lcd.print(SETTINGS.tidalPeak);
         lcd.setCursor(13, 1);
-        POTS.RESPRATE.update();
-        SETTINGS.respRate = map(POTS.RESPRATE.getValue(), 0, 1023, 8, 40); //bpm
+        RESPRATE.update();
+        SETTINGS.respRate = map(RESPRATE.getValue(), 0, 1023, 8, 40); //bpm
         lcd.print(SETTINGS.respRate);
         lcd.setCursor(14, 2);
-        POTS.IERATIO.update();
-        SETTINGS.IEratio = map(POTS.IERATIO.getValue(), 0, 1023, 1, 4);
+        IERATIO.update();
+        SETTINGS.IEratio = map(IERATIO.getValue(), 0, 1023, 1, 4);
         lcd.print(SETTINGS.IEratio);
         lcd.setCursor(11, 3);
-        POTS.TRIGGER.update();
-        SETTINGS.triggerSensitivity = map(POTS.TRIGGER.getValue(), 0, 1023, -1, -5); //?? cc
+        TRIGGER.update();
+        SETTINGS.triggerSensitivity = map(TRIGGER.getValue(), 0, 1023, -1, -5); //?? cc
         lcd.print(SETTINGS.triggerSensitivity);
         delay(5);
     }
@@ -107,12 +112,14 @@ void setup() {
     recalcVars();
     stepper.setMaxSpeed(5000);
     stepper.setAcceleration(5000);
-    homeMotor();
-    flowTimer = t.every(constants.FLOW_MEASURE_INTERVAL, getFlow, -1);
+    Serial.print("\nHERE\n");
+    //homeMotor();
+    flowTimer = t.every(FLOW_MEASURE_INTERVAL, getFlow, -1);
 
 }
 
 void loop() {
+    Serial.print("ASDFASDF");
     t.update();
     stepper.run();
     if (count%5==0) {
@@ -137,29 +144,30 @@ void loop() {
             lcd.print("Trigger: ");
             lcd.clear();
             lcd.setCursor(13, 0);
-            POTS.TIDALPEAK.update();
-            SETTINGS.tidalPeak = map(POTS.TIDALPEAK.getValue(), 0, 1023, 200, 800); //cc
+            TIDALPEAK.update();
+            SETTINGS.tidalPeak = map(TIDALPEAK.getValue(), 0, 1023, 200, 800); //cc
             lcd.print(SETTINGS.tidalPeak);
             lcd.setCursor(13, 1);
-            POTS.RESPRATE.update();
-            SETTINGS.respRate = map(POTS.RESPRATE.getValue(), 0, 1023, 8, 40); //bpm
+            RESPRATE.update();
+            SETTINGS.respRate = map(RESPRATE.getValue(), 0, 1023, 8, 40); //bpm
             lcd.print(SETTINGS.respRate);
             lcd.setCursor(14, 2);
-            POTS.IERATIO.update();
-            SETTINGS.IEratio = map(POTS.IERATIO.getValue(), 0, 1023, 1, 4);
+            IERATIO.update();
+            SETTINGS.IEratio = map(IERATIO.getValue(), 0, 1023, 1, 4);
             lcd.print(SETTINGS.IEratio);
             lcd.setCursor(11, 3);
-            POTS.TRIGGER.update();
-            SETTINGS.triggerSensitivity = map(POTS.TRIGGER.getValue(), 0, 1023, -1, -5); //?? cc
+            TRIGGER.update();
+            SETTINGS.triggerSensitivity = map(TRIGGER.getValue(), 0, 1023, -1, -5); //?? cc
             lcd.print(SETTINGS.triggerSensitivity);
         }
     }
     stepper.run();
     if (count%10==0 && !potChanged){
-        checkPot()
+        checkPot();
     }
     count++;
 }
+
 
 void homeMotor() {
     Serial.println("Homing");
@@ -200,13 +208,14 @@ void homeMotor() {
     stepper.setCurrentPosition(0);
 }
 
+
 float getDiff() {
-    return ((analogRead(pins.diff)*.0049)-1)*.4-.59;
+    return ((analogRead(diff)*.0049)-1)*.4-.59;
 }
 
 void getFlow() {
     READINGS.pressure = getPressure();
-    if (READINGS.pressure >= constants.MAXPRESSURE) {
+    if (READINGS.pressure >= MAXPRESSURE) {
         alert();
     }
     READINGS.flowRate = getDiff();
@@ -227,10 +236,10 @@ float getPressure(){
 }
 
 void alert(){
-    while (digitalRead(pins.buttonA) == HIGH){
-        digitalWrite(pins.buzzer, HIGH);
+    while (digitalRead(buttonA) == HIGH){
+        digitalWrite(buzzer, HIGH);
         delay(200);
-        digitalWrite(pins.buzzer, LOW);
+        digitalWrite(buzzer, LOW);
         delay(200);
     }
 }
@@ -244,35 +253,35 @@ void recalcVars(){
 }
 
 void checkPot(){
-    static POTS pot = TIDALPEAK;
+    static int pot = 1;
     switch(pot) {
-        case TIDALPEAK:
-            POTS.TIDALPEAK.update();
-            if (abs(SETTINGS.tidalPeak - map(POTS.TIDALPEAK.getValue(), 0, 1023, 200, 800)) > 20){
+        case 1:
+            TIDALPEAK.update();
+            if (abs(SETTINGS.tidalPeak - map(TIDALPEAK.getValue(), 0, 1023, 200, 800)) > 20){
                 potChanged=true;
             }
-            pot = RESPRATE;
+            pot = 2;
             break;
-        case RESPRATE:
-            POTS.RESPRATE.update();
-            if (abs(SETTINGS.respRate - map(POTS.RESPRATE.getValue(), 0, 1023, 8, 40)) > 1){
+        case 2:
+            RESPRATE.update();
+            if (abs(SETTINGS.respRate - map(RESPRATE.getValue(), 0, 1023, 8, 40)) > 1){
                 potChanged=true;
             }
-            pot = IERATIO;
+            pot = 3;
             break;
-        case IERATIO:
-            POTS.IERATIO.update();
-            if (abs(SETTINGS.IEratio - map(POTS.IERATIO.getValue(), 0, 1023, 1, 4)) > 0) {
+        case 3:
+            IERATIO.update();
+            if (abs(SETTINGS.IEratio - map(IERATIO.getValue(), 0, 1023, 1, 4)) > 0) {
                 potChanged=true;
             }
-            pot = TRIGGER;
+            pot = 4;
             break;
-        case TRIGGER:
-            POTS.TRIGGER.update();
-            if (abs(SETTINGS.triggerSensitivity - map(POTS.TRIGGER.getValue(), 0, 1023, -1, -5)) > > 0) {
+        case 4:
+            TRIGGER.update();
+            if (abs(SETTINGS.triggerSensitivity - map(TRIGGER.getValue(), 0, 1023, -1, -5)) > 0) {
                 potChanged=true;
             }
-            pot = TIDALPEAK;
+            pot = 1;
             break;
     }
 }
@@ -280,7 +289,7 @@ void checkPot(){
 void exhale() {
     count = 0;
     READINGS.PEEP = getPressure();
-    int startHold = millis();
+    //int startHold = millis();
     /*while (millis()-startHold < VARS.Thold){
         if (getPressure() > READINGS.PEEP){
             stepper.moveTo(stepper.currentPosition()-10);
@@ -298,7 +307,7 @@ void exhale() {
         // HOLD
     }
     READINGS.totalVol = 0;
-    flowTimer = t.every(constants.FLOW_MEASURE_INTERVAL, getFlow, -1);
+    flowTimer = t.every(FLOW_MEASURE_INTERVAL, getFlow, -1);
     lcd.clear();
 
 }
